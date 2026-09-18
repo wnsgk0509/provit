2ee94aa04a5acc23abc645a4155ccbd6659cd912
Seung-min <peppyksm@gmail.com>
2026-09-18 16:43:45 +0900

    fix: mapper 파일구조 변경

--- backend/src/main/java/com/provit/dao/interview/impl/InterviewDAOImpl.java
+++ backend/src/main/java/com/provit/dao/interview/impl/InterviewDAOImpl.java
@@ -22,8 +22,15 @@ import com.provit.dto.user.UserJobPreferenceDTO;
 @Repository
 public class InterviewDAOImpl implements InterviewDAO {
 
-    private static final String USER_NAMESPACE = "com.provit.mapper.UserInterviewMapper";
-    private static final String INTERVIEW_NAMESPACE = "com.provit.mapper.InterviewMapper";
+    private static final String PORTFOLIO_NAMESPACE = "com.provit.mapper.interview.PortfolioMapper";
+    private static final String COVER_LETTER_NAMESPACE = "com.provit.mapper.interview.CoverLetterMapper";
+    private static final String RESUME_NAMESPACE = "com.provit.mapper.interview.ResumeMapper";
+    private static final String EDUCATION_NAMESPACE = "com.provit.mapper.interview.EducationMapper";
+    private static final String CAREER_NAMESPACE = "com.provit.mapper.interview.CareerMapper";
+    private static final String CERTIFICATION_NAMESPACE = "com.provit.mapper.interview.CertificationMapper";
+    private static final String USER_JOB_NAMESPACE = "com.provit.mapper.interview.UserJobMapper";
+    private static final String INTERVIEW_HISTORY_NAMESPACE = "com.provit.mapper.interview.InterviewHistoryMapper";
+    private static final String INTERVIEW_RESULT_NAMESPACE = "com.provit.mapper.interview.InterviewResultMapper";
 
     private final SqlSession sqlSession;
 
@@ -34,83 +41,85 @@ public class InterviewDAOImpl implements InterviewDAO {
 
     @Override
     public PortfolioDTO selectPortfolioByUserNum(int userNum) {
-        return sqlSession.selectOne(USER_NAMESPACE + ".selectPortfolioByUserNum", userNum);
+        return sqlSession.selectOne(PORTFOLIO_NAMESPACE + ".selectPortfolioByUserNum", userNum);
     }
 
     @Override
     public CoverLetterDTO selectCoverLetterByUserNum(int userNum) {
-        return sqlSession.selectOne(USER_NAMESPACE + ".selectCoverLetterByUserNum", userNum);
+        return sqlSession.selectOne(COVER_LETTER_NAMESPACE + ".selectCoverLetterByUserNum", userNum);
     }
 
     @Override
     public List<ResumeDTO> selectResumeListByUserNum(int userNum) {
-        return sqlSession.selectList(USER_NAMESPACE + ".selectResumeListByUserNum", userNum);
+        return sqlSession.selectList(RESUME_NAMESPACE + ".selectResumeListByUserNum", userNum);
     }
 
     @Override
     public ResumeDTO selectResumeByResumeNumAndUserNum(int resumeNum, int userNum) {
         return sqlSession.selectOne(
-                USER_NAMESPACE + ".selectResumeByResumeNumAndUserNum",
+                RESUME_NAMESPACE + ".selectResumeByResumeNumAndUserNum",
                 createInterviewKeyMap(resumeNum, userNum));
     }
 
     @Override
     public List<EducationDTO> selectEducationListByResumeNum(int resumeNum) {
-        return sqlSession.selectList(USER_NAMESPACE + ".selectEducationListByResumeNum", resumeNum);
+        return sqlSession.selectList(EDUCATION_NAMESPACE + ".selectEducationListByResumeNum", resumeNum);
     }
 
     @Override
     public List<CareerDTO> selectCareerListByResumeNum(int resumeNum) {
-        return sqlSession.selectList(USER_NAMESPACE + ".selectCareerListByResumeNum", resumeNum);
+        return sqlSession.selectList(CAREER_NAMESPACE + ".selectCareerListByResumeNum", resumeNum);
     }
 
     @Override
     public List<CertificationDTO> selectCertificationListByResumeNum(int resumeNum) {
-        return sqlSession.selectList(USER_NAMESPACE + ".selectCertificationListByResumeNum", resumeNum);
+        return sqlSession.selectList(CERTIFICATION_NAMESPACE + ".selectCertificationListByResumeNum", resumeNum);
     }
 
     @Override
     public UserJobPreferenceDTO selectUserJobPreferenceByUserNum(int userNum) {
-        return sqlSession.selectOne(USER_NAMESPACE + ".selectUserJobPreferenceByUserNum", userNum);
+        return sqlSession.selectOne(USER_JOB_NAMESPACE + ".selectUserJobPreferenceByUserNum", userNum);
     }
 
     @Override
     public int selectNextHistoryNum() {
-        return sqlSession.selectOne(INTERVIEW_NAMESPACE + ".selectNextHistoryNum");
+        return sqlSession.selectOne(INTERVIEW_HISTORY_NAMESPACE + ".selectNextHistoryNum");
     }
 
     @Override
     public int insertInterviewHistory(InterviewHistoryDTO history) {
-        return sqlSession.insert(INTERVIEW_NAMESPACE + ".insertInterviewHistory", history);
+        return sqlSession.insert(INTERVIEW_HISTORY_NAMESPACE + ".insertInterviewHistory", history);
     }
 
     @Override
     public int insertInterviewResult(InterviewResultDTO result) {
-        return sqlSession.insert(INTERVIEW_NAMESPACE + ".insertInterviewResult", result);
+        return sqlSession.insert(INTERVIEW_RESULT_NAMESPACE + ".insertInterviewResult", result);
     }
 
     @Override
     public List<InterviewResultDTO> selectInterviewResultListByUserNum(int userNum) {
-        return sqlSession.selectList(INTERVIEW_NAMESPACE + ".selectInterviewResultListByUserNum", userNum);
+        return sqlSession.selectList(INTERVIEW_RESULT_NAMESPACE + ".selectInterviewResultListByUserNum", userNum);
     }
 
     @Override
     public InterviewHistoryDTO selectInterviewHistory(int historyNum, int userNum) {
         return sqlSession.selectOne(
-                INTERVIEW_NAMESPACE + ".selectInterviewHistory",
+                INTERVIEW_HISTORY_NAMESPACE + ".selectInterviewHistory",
                 createInterviewKeyMap(historyNum, userNum));
     }
 
     @Override
     public InterviewResultDTO selectInterviewResult(int historyNum, int userNum) {
         return sqlSession.selectOne(
-                INTERVIEW_NAMESPACE + ".selectInterviewResult",
+                INTERVIEW_RESULT_NAMESPACE + ".selectInterviewResult",
                 createInterviewKeyMap(historyNum, userNum));
     }
 
     @Override
     public InterviewResultDTO selectLatestInterviewResultByUserNum(int userNum) {
-        return sqlSession.selectOne(INTERVIEW_NAMESPACE + ".selectLatestInterviewResultByUserNum", userNum);
+        return sqlSession.selectOne(
+                INTERVIEW_RESULT_NAMESPACE + ".selectLatestInterviewResultByUserNum",
+                userNum);
     }
 
     private Map<String, Object> createInterviewKeyMap(int number, int userNum) {
